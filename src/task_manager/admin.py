eed10e7e21eaf7cd8bc02370addb6b1ee0538332
Denis <s.1ned@yandex.ru>
2026-04-08 22:59:14 +0300

    changes in admin.py

--- src/task_manager/admin.py
+++ src/task_manager/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from task_manager.models import Tasks, Comments, ProjectDetails, Tags, Projects, Attachments
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
+# inline
+class CommentInline(admin.TabularInline):
+    model = Comments
+    extra = 1
+
+class TagInline(admin.TabularInline):
+    model = Tags.tasks.through
+    extra = 1
+    # filter_horizontal = ('tasks',)
 
 # @admin.register(Tasks)
 class TasksAdmin(admin.ModelAdmin):
@@ -25,12 +36,32 @@ class TasksAdmin(admin.ModelAdmin):
         }),
     )
     exclude = ('is_reopened',)
-    list_display = ("name", "status", "priority", "project", "assignee")
-    list_display_links = ("name",)
+    list_display = (
+        "display_name",
+        "status",
+        "priority",
+        "project",
+        "assignee",
+        "priority_status"
+    )
+    list_display_links = ("display_name",)
     # list_display_links = ("name", "status")
     list_editable = ("status", "priority")
     readonly_fields = ("created_at",)
     list_filter = ("status","priority","project")
+    inlines = (CommentInline, TagInline )
+    def priority_status(self, obj):
+        if obj.priority < 3:
+            return "LOW"
+        if obj.priority < 5:
+            return "MEDIUM"
+        return "HIGH"
+    priority_status.string = ""
+    priority_status.short_description = "Приоритет статуса"
+
+    @admin.display(description='Наименование')
+    def display_name(self, instance):
+        return mark_safe(f"<h1>{instance.name}</h1>")
 
 
 class ProjectsAdmin(admin.ModelAdmin):
